a941d462853e6ad434040316d1f5a5a6e7b235ed
Nick Roach <njlroach@gmail.com>
2021-06-22 19:33:38 +1000

    Failed attempt to make functions independent and use map

--- scripts/DOM.js
+++ scripts/DOM.js
@@ -1,11 +1,17 @@
-// import {translator} from "./translate.js";
+import {translator} from "./translate.js";
 
 //Adds event listeners for all elements with the classname textbox and makes them call the function "translator"
 for (let i = 0; i < document.getElementsByClassName("textbox").length; ++i) {
   document
     .getElementsByClassName("textbox")
     [i].addEventListener("input", function () {
-      translator(event.target.value, event.target.id);
+      if(event.target.id = "letters"){
+        displayText("morse", translator(event.target.value, event.target.id));
+      }
+      if(event.target.id = "morse"){
+        displayText("letters", translator(event.target.value, event.target.id));
+      }
+      
     });
 }
 
--- scripts/translate.js
+++ scripts/translate.js
@@ -1,13 +1,14 @@
 function translator(input, id){
+
     let wordsArray = [];
     let morseArray = [];
     let lettersArray = [];
     let morseWords = [];
     let lettersString = "";
     let morseString = "";
-    console.clear();
+    // console.clear();
     
-    //if something has been modified in the letters box
+    // if something has been modified in the letters box
     if(id === "letters") {
         lettersString = input;
                 
@@ -15,27 +16,33 @@ function translator(input, id){
         const myRE = /[a-z]|[A-Z]|[0-9]|[ .,?'!]/;
         if(!myRE.test(input[input.length-1])){
             lettersString = lettersString.slice(0, -1); //this removes the unsupported character that has just been input so that it never displays
-            displayText("letters", lettersString);
-            alert(`The character \"${input[input.length-1]}\" is not supported`);
+            // displayText("letters", lettersString);
+            // alert(`The character \"${input[input.length-1]}\" is not supported`);
         }
         
         lettersString = lettersString.toLowerCase(); //uppercase and lowercase have the same morse codes
         wordsArray = lettersString.split(" ");
-        // console.log("lettersArray: " +lettersArray);
         
-        wordsArray.forEach(word => {
+        console.log(typeof(wordsArray));
+
+        morseArray = wordsArray.map(word => {
             let characters = word.split("");
 
-            characters.forEach(char => {
-                morseArray.push(cypher[char] + " "); //add one space between letters
+            let newWord = characters.map(char => {
+                return cypher[char] + " "; //add one space between letters
             });
 
-            morseArray.push("   "); //add three more spaces between words (there is already one after the final letter)
+            // newWord = newWord + "   ";
+            return newWord;
+            // morseArray.push("   "); //add three more spaces between words (there is already one after the final letter)
         });
         
-        morseString = morseArray.join("");
+        
+        console.log("Morse array: " + morseArray);
+        console.log(typeof(morseArray));
+        morseString = String(morseArray);
         morseString = morseString.slice(0, -4); //remove the four trailing spaces
-        displayText("morse", morseString);
+        return(morseString);
     }
 
 
@@ -43,48 +50,40 @@ function translator(input, id){
     //if something has been modified in the Morse Code box
     if(id === "morse") {
         morseString = input;
-
+        
         //check to make sure the most recently input character is either a dot, dash or space
         const myRE = /[.\- ]/;
         if(input.length > 0 && !myRE.test(input[input.length-1])){
             morseString = morseString.slice(0, -1); //this removes the unsupported character that has just been input so that it never displays
-            displayText("morse", morseString);
-            
-            alert("Please enter only the characters \".\", \"-\" and \"space\"");
+            // alert("Please enter only the characters \".\", \"-\" and \"space\"");
         }
 
-        if (morseString.slice(-1) === " " && morseString.slice(-2) != "  "){
-            console.log("|" + morseString + "|");
-            morseString = morseString + "   ";
-            console.log("|" + morseString +"|");
-        }
-
-
-
+        // if (morseString.slice(-1) === " " && morseString.slice(-2) != "  "){
+        //     console.log("|" + morseString + "|");
+        //     morseString = morseString + "   ";
+        //     console.log("|" + morseString +"|");
+        // }
 
         morseWords = morseString.split("    "); //split the morse code into words
 
-        morseWords.forEach(word => {
+        wordsArray = morseWords.map(word => {
             let characters = word.split(" "); //split the words into Morse character codes
 
-            characters.forEach(char => {
+            lettersArray = characters.map(char => {
                 if(reverseCypher[char]){
-                    lettersArray.push(reverseCypher[char]);
+                    return reverseCypher[char];
                 }
-                else lettersArray.push("[ ]");
+                else return "[ ]";
             });
 
             lettersArray.push(" "); //add a space between the words
         });
 
         lettersString = lettersArray.join("");
-       
         lettersString = lettersString.slice(0, -1); //remove the trailing space
-
-        console.log(morseString.slice(-4));
-
-        displayText("letters", lettersString);
+        
+        return(lettersString);
     }
 }
 
-// export {translator}
+export {translator}
